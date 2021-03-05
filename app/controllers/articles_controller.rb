61384d3e7bf606bc424e7ddfba944a1c211e644d
Rebecca Ross <b.laurenross@gmail.com>
2021-03-05 23:33:09 +0000

    adds update and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,9 +23,15 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
